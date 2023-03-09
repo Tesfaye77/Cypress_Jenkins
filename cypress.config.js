6081f536c7c76bbe8c161fb02ee7e3cdf0dfbbe6
unknown <Tesfaye77>
2023-03-09 09:38:27 +0100

    seconddd

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/report",
+    charts: true,
+    reportPageTitle: 'Ulrich Demo Report',
+    embeddedScreenshots: true
+  },
+  chromeWebSecurity: false,
+    $schema: 'https://on.cypress.io/cypress.schema.json',
+    projectId: ioceso,
+    experimentalStudio: true,
+    screenshotOnRunFailure: true,
+    
   e2e: {
     experimentalStudio: true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
      
       
@@ -12,18 +26,8 @@ module.exports = defineConfig({
   },
 
   
-    "chromeWebSecurity": false,
-    "$schema": "https://on.cypress.io/cypress.schema.json",
-    "projectId": "ioceso",
-    "experimentalStudio": true,
-    "screenshotOnRunFailure": true,
-    "reporter": "cypress-mochawesome-reporter",
-    "reporterOptions": {
-        "reportDir": "cypress/report",
-        "charts": true,
-        "reportPageTitle": "Ulrich Demo Report",
-        "embeddedScreenshots": true
-      }
+    
+    
 
 
 
