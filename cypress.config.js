80441dbef83991d35c91e82ec9226dc42b38b9b8
unknown <Tesfaye77>
2023-03-09 09:21:29 +0100

    secondd

--- cypress.config.js
+++ cypress.config.js
@@ -10,4 +10,22 @@ module.exports = defineConfig({
     },
     specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}'
   },
+
+  
+    "chromeWebSecurity": false,
+    "$schema": "https://on.cypress.io/cypress.schema.json",
+    "projectId": "ioceso",
+    "experimentalStudio": true,
+    "screenshotOnRunFailure": true,
+    "reporter": "cypress-mochawesome-reporter",
+    "reporterOptions": {
+        "reportDir": "cypress/report",
+        "charts": true,
+        "reportPageTitle": "Ulrich Demo Report",
+        "embeddedScreenshots": true
+      }
+
+
+
 });
+
